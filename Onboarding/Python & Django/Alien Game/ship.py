5c9125a73ce5261b0b361f061138720e4e9d4b80
Kai Zhang <kaizen.agility@gmail.com>
2022-09-08 19:29:59 -0400

    fixed issue

--- Onboarding/Python & Django/Alien Game/ship.py
+++ Onboarding/Python & Django/Alien Game/ship.py
@@ -11,19 +11,19 @@ class Ship:
         self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
-        # Store a decimal value for the ship's horizontal position.
-        self.x = float(self.rect.x)
-
-        # Movement flag
-        self.moving_right = False
-        self.moving_left = False
-
         # Load the ship image and get its rect.
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
 
         # Start each new ship at the bottom center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
+
+        # Store a decimal value for the ship's horizontal position.
+        self.x = float(self.rect.x)
+
+        # Movement flag
+        self.moving_right = False
+        self.moving_left = False
 
     def update(self):
         """Update the ship's position based on the movement flag."""
