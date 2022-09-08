94cafa65422e3beb40022f901a521b8e29a2f959
Kai Zhang <kaizen.agility@gmail.com>
2022-09-08 02:45:26 -0400

    key up while loop

--- Onboarding/Python & Django/Alien Game/ship.py
+++ Onboarding/Python & Django/Alien Game/ship.py
@@ -10,6 +10,9 @@ class Ship:
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
 
+        # Movement flag
+        self.moving_right = False
+
         # Load the ship image and get its rect.
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
@@ -20,3 +23,9 @@ class Ship:
     def blitme(self):
         """Draw the ship at its current location."""
         self.screen.blit(self.image, self.rect)
+
+    def update(self):
+        """Update the ship's position based on the movement flag."""
+
+        if self.moving_right:
+            self.rect.x += 1
