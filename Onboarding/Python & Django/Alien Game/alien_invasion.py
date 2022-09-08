85dd16173e8f0211e4529b795171fca5f885c7c1
Kai Zhang <kaizen.agility@gmail.com>
2022-09-08 00:34:49 -0400

    Update alien_invasion.py

--- Onboarding/Python & Django/Alien Game/alien_invasion.py
+++ Onboarding/Python & Django/Alien Game/alien_invasion.py
@@ -15,7 +15,7 @@ class AlienInvasion:
         while True:
             # Watch for keyboard and mouse events.
             for event in pygame.event.get():
-                if event.type = pygame.QUIT:
+                if event.type == pygame.QUIT:
                     sys.exit()
 
         # Make the most recently drawn screen visible.
