bb9cf184bedb3bc36062fdd17d4fc3c851ae1054
Kai Zhang <kaizen.agility@gmail.com>
2022-11-13 12:50:31 -0500

    ThemeContext as provider

--- Online Learning/Udemy/React_with_Redux/app1/src/FunctionContextComponent.js
+++ Online Learning/Udemy/React_with_Redux/app1/src/FunctionContextComponent.js
@@ -1,5 +1,15 @@
-export default function FunctionContextComponent() {
-    
-    return
+import React, { useContext } from 'react'
+import { ThemeContext } from './App'
 
+export default function FunctionContextComponent() {
+    const darkTheme = useContext(ThemeContext)
+    const themeStyles = {
+            backgroundColor: darkTheme ? '#333' : '#CCC',
+            color: darkTheme ? '#CCC' : '#333',
+            padding: '2rem',
+            margin: '2rem'
+    }
+    return (
+        <div style={themeStyles}> Function Theme </div>
+    )
 }
---
+++ Online Learning/Udemy/React_with_Redux/app1/src/ThemeContext.js
@@ -0,0 +1,19 @@
+import React, {useContext, useState} from 'react'
+
+const ThemeContext = React.createContext()
+
+export function ThemeProvider({children}) {
+
+    const [darkTheme, setDarkTheme] = useState(true)
+
+    function toggleTheme() {
+        setDarkTheme(prevDarkTheme => !prevDarkTheme)
+    }
+
+    return (
+        <ThemeContext.Provider value={darkTheme}>
+            {children}
+        </ThemeContext.Provider>
+    )
+
+}
