abd69e0d92253719943feca2d138a5455040e242
Kai Zhang <kaizen.agility@gmail.com>
2022-11-22 13:25:01 -0500

    branch 4: create handleLikes button as a React hook in Post component

--- React_Curriculum/day1_practice_repo/example/src/Post.js
+++ React_Curriculum/day1_practice_repo/example/src/Post.js
@@ -1,14 +1,21 @@
 import './App.css'
+import { useState } from 'react'
 
 const Post = (props) => {
     const {name, status, time, likes} = props.post
-    console.log(name)
+    const [hearts, setHearts] = useState(likes)
+
+    const handleLikes = () => {
+        setHearts(hearts + 1)
+    }
+
     return (
         <>
             <h4>{name}</h4>
             <h5>{time}</h5>
             <p>{status}</p>
-            <button>💙 {likes}</button>
+            {/* <button onClick={() => setHearts(hearts+1)}>💙 {hearts}</button> */}
+            <button onClick={() => handleLikes()}>💙 {hearts}</button>
             <hr />
         </>
     )
